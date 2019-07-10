b179a83e178787b4e026799d8d5fb934b0f217c8
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-10 14:13:11 +0100

    Route Query and Name

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,19 +1,10 @@
 <template>
     <nav class="nav has-shadow">
       <div class="container">
-        <router-link to="/" exact>
-          <img src="http://bit.ly/vue-img"
-            alt="Vue SPA" />
-        </router-link>
-        <router-link class="nav-item is-tab" to="/category/front-end" exact>
-          Front-end
-        </router-link>
-        <router-link class="nav-item is-tab" to="/category/mobile" exact>
-          Mobile
-        </router-link>
-        <router-link class="nav-item is-tab" to="/login">
-          Login
-        </router-link>
+        <router-link to="/" exact><img src="http://bit.ly/vue-img" alt="Vue SPA" /></router-link>
+        <router-link class="nav-item is-tab" to="/category/front-end">Front-end</router-link>
+        <router-link class="nav-item is-tab" :to="{ name: 'category', params: { id: 'mobile' } }">Mobile</router-link>
+        <router-link class="nav-item is-tab" to="/login">Login</router-link>
       </div>
     </nav>
 </template>
