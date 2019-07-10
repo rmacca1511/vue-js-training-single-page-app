60bc13399467f3a041d96c0d5ea64dab5b4ffe7f
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-10 10:52:57 +0100

    LINTING, PLEASE READ BEFORE STARTING KAGOOL WORK

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
+import Vue from 'vue'
 
 const app = new Vue({
   data: {
-    hello: "hi there7"
+    hello: 'hi there'
   },
   template: '<div id="app">{{ hello }}</div>'
-});
+})
 
-export { app };
+export { app }
