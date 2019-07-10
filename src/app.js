3eec1a490b602ef76a08d28e8d1b3a00cd830783
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-10 11:11:37 +0100

    Conversion to .vue files!

--- src/app.js
+++ src/app.js
@@ -1,32 +1,8 @@
 import Vue from 'vue'
-
-Vue.component('app', {
-  template: `
-    <div id="app">
-      <nav class="nav has-shadow">
-        <div class="container">
-          <a href="/">
-            <img src="http://bit.ly/vue-img"
-              alt="Vue SPA" />
-          </a>
-        </div>
-      </nav>
-      <section class="main-section section"></section>
-      <footer class="footer">
-        <div class="container">
-          <div class="content has-text-centered">
-            Follow us on
-            <a href="https://twitter.com/bstavroulakis"
-            target="_blank">Twitter</a>
-          </div>
-        </div>
-      </footer>
-    </div>
-  `
-})
+import AppLayout from './theme/Layout.vue'
 
 const app = new Vue({
-  render: h => h('app')
+  render: h => h(AppLayout)
 })
 
 export { app }
