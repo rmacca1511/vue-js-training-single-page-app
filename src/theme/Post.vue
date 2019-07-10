3d4f2824aec2d8848bc92794b16f72296c0e0d21
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-10 11:51:28 +0100

    Slots

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="card">
     <div class="card-content">
-      <h3>{{ post.title }}</h3>
-      {{ post.content }}
+      <slot name="title"></slot>
+      <slot name="content"></slot>
     </div>
     <footer class="card-footer">
-      <a class="card-footer-item" :href="post.link" target="_blank">Read More</a>
+      <a class="card-footer-item" :href="link" target="_blank">Read More</a>
     </footer>
   </div>
 </template>
