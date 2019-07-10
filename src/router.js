977025c4341c5b5ca59c3e874f827697b96d70b1
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-10 14:23:29 +0100

    Lazy Loading

--- src/router.js
+++ src/router.js
@@ -4,6 +4,11 @@ import Category from './theme/Category.vue'
 import Login from './theme/Login.vue'
 import NotFound from './theme/NotFound.vue'
 
+// For lazy loading async imports, useful for when app increases in size, have this in mind whilst client side rendering
+// const Category = () => System.import('./theme/Category.vue')
+// const Login = () => System.import('./theme/Login.vue')
+// const NotFound = () => System.import('./theme/NotFound.vue')
+
 Vue.use(VueRouter)
 
 // History mode means we can use localhost:3000/login instead of localhost:3000/#/login
