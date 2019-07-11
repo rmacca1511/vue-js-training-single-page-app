185f583e81a0534a3b8edc01e950442e382cc786
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-11 09:18:24 +0100

    Bundle Renderer

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path");
+const { createBundleRenderer } = require('vue-server-renderer')
+let renderer
 
 const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
@@ -9,11 +11,19 @@ const indexHTML = (() => {
 
 app.use("/dist", express.static(path.resolve(__dirname, "./dist")));
 
-require("./build/dev-server")(app);
+require("./build/dev-server")(app, bundle => {
+  renderer = createBundleRenderer(bundle)
+});
 
 app.get('*', (req, res) => {
-  res.write(indexHTML);
-  res.end();
+  renderer.renderToString({ url: req.url }, (err, html) => {
+    if (err) {
+      return res.status(500).send('Server Error')
+    }
+    html = indexHTML.replace('{{ APP }}', html)
+    res.write(html)
+    res.end();
+  })
 });
 
 const port = process.env.PORT || 3000;
