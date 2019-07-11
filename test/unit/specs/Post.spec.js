9f0da4639106712d63aa132f5a03a6274f0c5b74
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-11 10:34:09 +0100

    Testing Component Changes

--- test/unit/specs/Post.spec.js
+++ test/unit/specs/Post.spec.js
@@ -2,14 +2,31 @@ import Vue from 'vue'
 import Post from '../../../src/theme/Post.vue'
 
 describe('Post.vue', () => {
-  it('should render the link', () => {
+  const createComponent = () => {
     const PostConstructer = Vue.extend(Post)
     const comp = new PostConstructer({
       propsData: {
         link: 'http://www.pluralsight.com'
       }
     }).$mount()
+    return comp
+  }
+  it('should render the link', () => {
+    const comp = createComponent()
     expect(comp.$el.querySelector('.card-footer-item').getAttribute('href'))
       .to.equal('http://www.pluralsight.com')
+  })
+  it('should update element\'s href when property link changes', (done) => {
+    const comp = createComponent()
+
+    expect(comp.$el.querySelector('.card-footer-item').getAttribute('href'))
+      .to.equal('http://www.pluralsight.com')
+
+    comp.link = 'http://fullstackweekly.com'
+    Vue.nextTick(() => {
+      expect(comp.$el.querySelector('.card-footer-item').getAttribute('href'))
+        .to.equal('http://fullstackweekly.com')
+      done()
+    })
   })
 })
